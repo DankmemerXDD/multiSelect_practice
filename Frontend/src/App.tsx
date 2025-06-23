c216ad0b6e05931fb0432fa7f14e497236d4c263
DankmemerXDD <kims9725@hotmail.com>
2025-06-23 14:43:28 +0200

    Select component and styling
    added component select.tsx and css modules styling

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,21 @@
+import {Select} from "./components/Select"
+
+
+const options = [
+  { label: "First", value: 1}, 
+  { label: "Second", value: 2}, 
+  { label: "Third", value: 3}, 
+  { label: "Fourth", value: 4}, 
+  { label: "Fifth", value: 5}, 
+  
+]
+
 
 function App() {
- 
   return (
-    <h1>hi</h1>)
+    <Select options={options}/>
+
+    )
   }
     
 export default App
